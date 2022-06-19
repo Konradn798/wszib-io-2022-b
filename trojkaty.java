12c2c8c0137837eeeac4c4b2a64399f7378347fb
Konradn798 <konrad.n798@gmail.com>
2022-06-19 11:03:50 +0200

    Optymalizacja: uwspólnienie kodu

--- trojkaty.java
+++ trojkaty.java
@@ -50,7 +50,8 @@ class Trojkaty {
                    if (a * a + b * b == c*c) return true;
                    if (b * b + c * c == a*a) return true;
                    if (a * a + c * c == b*b) return true;
-                   return false;
+                   
+		   return false;
                }
                ...
                public static void jakiTrojkat(float a, float b, float c){
